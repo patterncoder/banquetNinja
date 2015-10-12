ee2b223b04030f4e5a16c93fbb68eb7970922f65
patterncoder <chris@oldtowndining.com>
2015-10-12 08:01:25 -0700

    worked on webpack config

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ var port = isProduction ? process.env.PORT : 3000;
 var publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
-app.get('*', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
+
+console.log(isProduction + "this is the variable");
 // We only want to run the workflow when not in production
 if (!isProduction) {
-
+    console.log('intheifstatement');
   // We require the bundler inside the if block because
   // it is only needed in a development environment. Later
   // you will see why this is a good idea
@@ -25,12 +24,19 @@ if (!isProduction) {
   // Any requests to localhost:3000/build is proxied
   // to webpack-dev-server
   app.all('/build/*', function (req, res) {
+    console.log('a request that will be proxied');
+    console.log(req);
     proxy.web(req, res, {
         target: 'http://localhost:8080'
     });
   });
+  
 
 }
+
+app.get('*', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 // It is important to catch any errors from the proxy or the
 // server will crash. An example of this is connecting to the
