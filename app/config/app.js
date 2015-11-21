7ccce3db73611027d1058b98e752bb6d81936bed
Christopher Baily <Christopher Baily>
2015-11-20 19:31:50 -0800

    trying an api wakeup strategy

--- app/config/app.js
+++ app/config/app.js
@@ -3,6 +3,7 @@ import angular from 'angular';
 import Shell from '../shell';
 import Features from '../features';
 import Common from '../common';
+import config from 'config';
 
 //think import config should be deleted
 //import config from 'config';
@@ -13,4 +14,10 @@ module.exports = angular.module('app', [Shell, Features, Common])
     .config(routing)
     .run(['$dataSource', function($dataSource){
         $dataSource.init();
+    }])
+    .run(['$http', function($http){
+        //wake up api server
+        $http.get(config.apiBase + '/wakeup', function(err, result){
+            console.log(result);
+        })
     }]);
