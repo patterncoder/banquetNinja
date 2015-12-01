b20bda873b168be92fb3a7ab06545f488ba2958f
patterncoder <patterncoder@gmail.com>
2015-12-01 11:08:00 -0800

    accidentally removed mongoose...addded it back

--- app/common/index.js
+++ app/common/index.js
@@ -2,10 +2,12 @@ import angular from 'angular';
 import tmNotifier from './tmNotifier';
 import toastr from 'toastr';
 import tmModalSvc from './tmModalSvc';
+import mongoose from 'mongoose';
 
 
 export default angular.module('common', [])
     .value('tmToastr', toastr)
+    .value('tmMongoose', mongoose)
     .factory('tmNotifier', tmNotifier)
     .service('tmModalSvc', tmModalSvc)
     .name;
