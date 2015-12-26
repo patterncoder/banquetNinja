9298b0792f6e4f7e1c763c651653a73b26fabdc8
patterncoder <patterncoder@gmail.com>
2015-12-26 10:08:54 -0800

    same note as previous

---
+++ app/common/tmDialogSvc.js
@@ -0,0 +1,59 @@
+import angular from 'angular';
+class tmDialogSvc{
+    constructor($mdDialog){
+        
+        this.$mdDialog = $mdDialog;
+        this.dialogDefaults = {
+            hasBackdrop: true,
+            escapeToClose: true,
+            fullscreen: false,
+            template: require('apply!./tmDialogSvc.jade')
+        };
+        this.dialogOptions = {
+            closeButtonText: 'Close',
+            actionButtonText: 'OK',
+            headerText: 'Proceed?',
+            bodyText: 'Perform this action?'
+        };
+        
+    }
+    
+    showDialog (customDialogDefaults, customDialogOptions) {
+        if(!customDialogDefaults) customDialogDefaults = {};
+            //customDialogDefaults.backdrop = 'static';
+            return this.show(customDialogDefaults, customDialogOptions);
+    }
+    
+    show (customDialogDefaults, customDialogOptions) {
+            var tempDialogDefaults = {};
+            var tempDialogOptions = {};
+            angular.extend(tempDialogDefaults, this.dialogDefaults, customDialogDefaults);
+            angular.extend(tempDialogOptions, this.dialogOptions, customDialogOptions);
+            
+            if(!tempDialogDefaults.controller){
+                tempDialogDefaults.controller = function($scope, $mdDialog){
+                    $scope.dialogOptions = tempDialogOptions;
+                    $scope.dialogOptions.ok = function(result){
+                        $mdDialog.hide(result);
+                    }
+                    $scope.dialogOptions.close = function(result){
+                        $mdDialog.cancel(result);
+                    };
+                };
+                // function ($scope, $modalInstance) {
+                //     $scope.modalOptions = tempModalOptions;
+                //     $scope.modalOptions.ok = function(result) {
+                //         $modalInstance.close(result);
+                //     };
+                //     $scope.modalOptions.close = function(result){
+                //         $modalInstance.dismiss('cancel');
+                //     };
+                // };
+            }
+            return this.$mdDialog.show(tempDialogDefaults);
+    }
+}
+
+tmDialogSvc.$inject = ['$mdDialog'];
+
+export default tmDialogSvc;
---
+++ app/features/production/menuitems/tmMenuItemDetailCtrl.1.js
@@ -0,0 +1,90 @@
+import angular from 'angular';
+import productionSchemas from '../../../../schemas/production';
+//import {menuitem as menuItemSchema} from '../../../../schemas/production';
+
+class tmMenuItemDetailCtrl {
+    constructor($dataSource, 
+            $stateParams, 
+            tmNotifier,
+            $modalInstance,
+            $state,
+            tmModalSvc,
+            $scope,
+            tmMongoose){
+        var self = this;
+        self.err = {};
+        self.$dataSource = $dataSource;
+        self.$scope = $scope;
+        self.$state = $state;
+        self.tmMongoose = tmMongoose;
+        self.$modalInstance = $modalInstance;
+        self.tmModalSvc = tmModalSvc;
+        self.MenuItem = $dataSource.load('MenuItem');
+        self.MenuItem.getOne($stateParams.id, true).then(function(data,status){
+            if(data.noData){
+                tmNotifier.notify('That request was not found');
+                self.$modalInstance.close();
+                $state.go('root.menuitems')
+            }
+            self.menuItem = new self.tmMongoose.Document(data,productionSchemas.menuitem);
+            //self.menuItem = data;
+            self.master = angular.copy(data);
+        });
+        
+    }
+    
+    reset(){
+        var self = this;
+        self.menuItem = angular.copy(self.master);
+        self.detailForm.$setPristine();
+    }
+    
+    close(){
+        var self = this;
+        var modalOptions = {
+                closeButtonText: 'No',
+                actionButtonText: 'Yes',
+                headerText: 'Wait!',
+                bodyText: 'Do you want to leave without saving??'
+            };
+            
+            if(self.detailForm.$pristine) {
+                self.$modalInstance.close();
+                self.$state.go('root.menuitems');
+            } 
+            else {
+                self.tmModalSvc.showModal({}, modalOptions).then(function(result){
+                    
+                    self.reset();
+                    self.$modalInstance.close();
+                    self.$state.go('root.menuitems');
+                });
+            }
+    }
+    
+    saveChanges(){
+        var self = this;
+        self.menuItem.validate(function(err){
+            if(err){
+                self.err = err;
+                console.log(self.err);
+                console.log(err);
+                self.$scope.$apply();
+                return;
+            }
+            console.log('theoretically I could be saved');
+        })
+    }
+}
+
+
+tmMenuItemDetailCtrl.$inject = ['$dataSource', 
+            '$stateParams', 
+            'tmNotifier', 
+            '$modalInstance',
+            '$state',
+            'tmModalSvc',
+            '$scope',
+            'tmMongoose'];
+
+export default tmMenuItemDetailCtrl;
