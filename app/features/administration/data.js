f1feabff9f2400a6b35c2669e1187d0cf9610ea8
crs2here <crs2here@gmail.com>
2017-06-07 17:02:43 -0700

    add api route for user update

--- app/features/administration/data.js
+++ app/features/administration/data.js
@@ -9,6 +9,13 @@ export default [
             methods: {update: { method: 'PUT', isArray: false},
                         query: { method: 'GET', isArray: false}}
         });
+        $dataSourceProvider.addApiRoute({
+            key: 'Users', 
+            url: config.apiBase + '/users/:_id', 
+            defaults: { _id: "@id" },
+            methods: {update: { method: 'PUT', isArray: false },
+                        query: { method: 'GET', isArray: false}}
+        });        
     }
 ]
 
