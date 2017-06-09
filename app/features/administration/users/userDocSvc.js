e20fe924c98feadf714e598efb694483e4b16424
crs2here <crs2here@gmail.com>
2017-06-08 17:31:37 -0700

    add crud ops for role on docsvc

--- app/features/administration/users/userDocSvc.js
+++ app/features/administration/users/userDocSvc.js
@@ -5,7 +5,21 @@ function userDocSvc (tmDocFactory) {
     
     this.__proto__ = tmDocFactory('Users', ninjaSchemas.account.User);
     
-    
+    this.addRole = function (role) {
+        this.doc.roles.push(role)
+    };
+    this.updateRole = function (oldRole, newRole) {
+        var idx = this.doc.roles.indexOf(oldRole);
+        if (idx >= 0) {
+            this.doc.roles[idx] = newRole;
+        }
+    };
+    this.removeRole = function (role) {
+        var idx = this.doc.roles.indexOf(role);
+        if (idx >= 0) {
+            this.doc.roles.splice(idx, 1);
+        }
+    };
     
     return this;
     
