3bdd91c36bacba915ec042deca8bad960552e1f7
crs2here <crs2here@gmail.com>
2017-06-18 11:39:04 -0700

    overwrite savechanges to update userroles in db

--- app/features/administration/users/tmUserDocSvc.js
+++ app/features/administration/users/tmUserDocSvc.js
@@ -20,6 +20,33 @@ function tmUserDocSvc (tmDocFactory) {
         console.log(`removeRole set arr to ${this.doc.roles}`);
     };
     
+    this.saveChanges = function () {
+        var self = this;
+        var deferred = this.$q.defer();
+        // depopulate for saving
+        var stripped = _.pick(self.doc, "_id");
+        self.doc.user = stripped._id;
+
+        var monDoc = new this.tmMongoose.Document(self.doc, self.docSchema);
+        monDoc.validate(function (err) {
+            if (err) {
+                console.log(err);
+                self.validationError = err;
+                console.log(self.validationError);
+                deferred.reject('user doc service has errors');
+                return;
+            }
+            self.docModel.update(self.doc).then(function (data) {
+                console.log("in here");
+                data = convertDateStrings(data);
+                self.doc = data;
+                self.master = angular.copy(data);
+                deferred.resolve();
+            });
+        });
+        return deferred.promise;
+    };
+    
     return this;
     
 }
