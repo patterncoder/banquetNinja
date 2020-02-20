ff08e2a33b2ac940522d67f978341cd4365b90b7
Jesse Parnell <jesse@baily.com>
2020-02-20 14:22:11 -0800

    reverting state of project after incorrectly testing another branch against stage

--- app/features/customers/customers/tmCustomerDetailCtrl.js
+++ app/features/customers/customers/tmCustomerDetailCtrl.js
@@ -50,24 +50,21 @@ function tmCustomerDetailCtrl(
     });
 
     this.getRelatedContracts = function () {
-        var self = this;
-        let relatedContracts = self.docSvc.doc.contracts.map(contract => `valueIn[_id]=${contract._id}`).join("&");
-        if (relatedContracts.length === 0) return;
-        let url = config.apiBase + '/events/contracts?select=eventDate%20status%20eventName&' + relatedContracts;
-        console.log("contracts api url:", url);
+      var self = this;
+      let relatedContracts = self.docSvc.doc.contracts.map(contract => `valueIn[_id]=${contract._id}`).join("&");
+      if (relatedContracts.length === 0) return;
+      let url =  config.apiBase + '/events/contracts?select=eventDate%20eventName&' + relatedContracts;
+      console.log("contracts api url:", url);
 
-        var req = {
-            method: 'GET',
-            url: url
-        };
-        $http(req).then(function (result) {
-            console.log("result:", result);
-            if (self.contractsList == undefined) {
-                self.contractsList = [];
-            }
-            self.contractsList = result.data.data;
-            console.log("getRelatedContracts: result:", result.data.data);
-        });
+      var req = {
+        method: 'GET',
+        url: url 
+      };
+      $http(req).then(function(result) {
+        console.log("result:", result);
+        self.contractsList = result.data.data;
+        console.log("getRelatedContracts: result:", result.data.data);
+      });
     }
 
     this.getDetailTitle = function () {
@@ -92,16 +89,12 @@ function tmCustomerDetailCtrl(
         self.tmDialogSvc.showDialog(dialogConfig).then(function (item) {
             Contract.add(item).then(function (item) {
                 self.docSvc.addContract(item);
-                self.docSvc.saveChanges().then(function () {
-                    if (self.contractsList == undefined) {
-                        self.contractsList = [];
-                    }
-
+                self.docSvc.saveChanges().then(function(){
                     self.contractsList.push(item);
                     self.docSvc.refreshFromServer();
                     //self.getRelatedContracts(); //we want to get the most recent contract that was just made...
                 });
-
+                
 
             });
         });
@@ -130,7 +123,7 @@ function tmCustomerDetailCtrl(
             } else {
                 self.docSvc.updateAddress(index, item);
             }
-            self.docSvc.saveChanges().then(function () {
+            self.docSvc.saveChanges().then(function(){
                 self.docSvc.refreshFromServer();
             });
 
@@ -160,7 +153,7 @@ function tmCustomerDetailCtrl(
             } else {
                 self.docSvc.updateEmail(index, item);
             }
-            self.docSvc.saveChanges().then(function () {
+            self.docSvc.saveChanges().then(function(){
                 self.docSvc.refreshFromServer();
             });
         });
@@ -188,7 +181,7 @@ function tmCustomerDetailCtrl(
             } else {
                 self.docSvc.updatePhoneNumber(index, item);
             }
-            self.docSvc.saveChanges().then(function () {
+            self.docSvc.saveChanges().then(function(){
                 self.docSvc.refreshFromServer();
             });
         });
