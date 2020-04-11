e975138b63dde9ae83fd9a8026a4592fd8485805
trillobite <jparnell0@protonmail.com>
2020-04-10 20:36:25 -0700

    edit title of section works

--- app/features/production/menus/tmMenuDocSvc.js
+++ app/features/production/menus/tmMenuDocSvc.js
@@ -1,23 +1,75 @@
 import ninjaSchemas from 'ninjaSchemas';
 import angular from 'angular';
 
-function tmMenuDocSvc (tmDocFactory) {
-    
+function tmMenuDocSvc(tmDocFactory) {
+
     this.__proto__ = tmDocFactory('Menu', ninjaSchemas.production.Menu);
 
-    this.doc.sections = this.doc["sections"] ? this.doc.sections : []; 
-    this.doc.sections.map((item) => {
-        item.visible = true;
-    });
-    
+    console.log("__proto__:", this.__proto__);
+
+    this.doc.sections = this.doc["sections"] ? this.doc.sections : [];
+    // this.doc.sections.map((item) => {
+    //     item.visible = true;
+    // });
+
+    this.activeObj = {
+        visible: 0,
+        index: 0
+    };
+
+    this.getCategories = () => {
+        //console.log("test", tmDocFactory("Lookups", ninjaSchemas.common.Lookups));
+
+        // let url = `${config.apiBase}/common/lookups?where[meta.company]=${this.searchCategory}&like[name]=${this.searchName}`;
+        // let request = {
+        //     method: "GET",
+        //     url: url
+        // };
+        // this.$http(request).then((data) => {
+        //     console.log("categories", data);
+        //     self.categories= data.data.data;
+        // });
+
+        // console.log("testing:", tmDocFactory);
+
+        //let lookups = tmDocFactory("Lookups", ninjaSchemas.common.Lookups).$dataSource.load('Lookups');
+
+        // not working yet, still trying to get lookups, so I can get a list of categories...
+        // console.log("Lookups:", this.$dataSource.load("Lookups"));
+        // this.getDetailTitle();
+        console.log("lookups schema:", ninjaSchemas.common.Lookups);
+        // try {
+        //     let tmp = ninjaSchemas.common.Lookups.query();
+        //     console.log("query:", tmp);
+        // } catch (e) {
+        //     console.log(e);
+        // }
+        let mylookups = this.$dataSource.load("Lookups");
+        console.log("mylookups:", mylookups);
+        try {
+            // mylookups.remove(undefined);
+            mylookups.getOne(ninjaSchemas.common.Lookups.$id).then((data) => {
+                console.log("data:", data);
+                console.log("lookups test");
+
+                console.log(mylookups.List);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+
+        // self.menuItemCategories = lookups.List.menuItemTags;
+        // console.log("menuItemCategories:", self.menuItemCategories)
+    };
+
     // title, subtitle, items, footer
-    this.addSection = function(section){
+    this.addSection = function (section) {
         var newSection = {
             title: "Section Title",
             subtitle: "Section subtitle",
             items: [],
             footer: "section footer",
-            visible: true
+            //visible: true
         }
         this.doc.sections.push(newSection);
     };
@@ -26,24 +78,39 @@ function tmMenuDocSvc (tmDocFactory) {
         let doc = this.doc.sections[index];
         console.log("editSection", index, doc);
         //doc.visible = false;
+        this.activeObj.visible = 1;
+        this.activeObj.index = index;
     };
-    
-    this.removeSection = function(index){
+
+    this.setActiveTab = (index) => {
+        this.activeObj.visible = index;
+    };
+
+    this.openAddFood = () => {
+        this.setActiveTab(2);
+    };
+
+    this.openEditSection = () => {
+        this.getCategories();
+        this.setActiveTab(1);
+    };
+
+    this.removeSection = function (index) {
         this.doc.sections.splice(index, 1);
     };
-    
-    this.updateSection = function(section){};
-    
-    this.addMenuItem = function(section, menuItem){};
-    
-    this.removeMenuItem = function(section, menuItem){};
-    
-    this.updateMenuItem = function(section, menuItem){};
-    
-    
-    
+
+    this.updateSection = function (section) { };
+
+    this.addMenuItem = function (section, menuItem) { };
+
+    this.removeMenuItem = function (section, menuItem) { };
+
+    this.updateMenuItem = function (section, menuItem) { };
+
+
+
     return this;
-    
+
 }
 
 
