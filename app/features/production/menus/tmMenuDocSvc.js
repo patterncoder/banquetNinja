6e7133ba7a6e5bc523ebbb4e4a1b815738167f72
trillobite <jparnell0@protonmail.com>
2020-04-23 23:54:50 -0700

    still trying to work out the bugs

--- app/features/production/menus/tmMenuDocSvc.js
+++ app/features/production/menus/tmMenuDocSvc.js
@@ -1,11 +1,13 @@
 import ninjaSchemas from 'ninjaSchemas';
 import angular from 'angular';
 
-function tmMenuDocSvc (tmDocFactory) {
-    
+function tmMenuDocSvc(tmDocFactory) {
+
     this.__proto__ = tmDocFactory('Menu', ninjaSchemas.production.Menu);
 
-    this.doc.sections = this.doc["sections"] ? this.doc.sections : []; 
+    console.log("__proto__:", this.__proto__);
+
+    this.doc.sections = this.doc["sections"] ? this.doc.sections : [];
     // this.doc.sections.map((item) => {
     //     item.visible = true;
     // });
@@ -24,22 +26,36 @@ function tmMenuDocSvc (tmDocFactory) {
         //     url: url
         // };
         // this.$http(request).then((data) => {
-        //     self.addableMenuItems = data.data.data;
+        //     console.log("categories", data);
+        //     self.categories= data.data.data;
         // });
 
         // console.log("testing:", tmDocFactory);
 
         //let lookups = tmDocFactory("Lookups", ninjaSchemas.common.Lookups).$dataSource.load('Lookups');
 
-        //not working yet, still trying to get lookups, so I can get a list of categories...
-        console.log("dataSource:", this.$dataSource.load("Lookups"));
+        // not working yet, still trying to get lookups, so I can get a list of categories...
+        // console.log("Lookups:", this.$dataSource.load("Lookups"));
+        // this.getDetailTitle();
+        console.log("lookups schema:", ninjaSchemas.common.Lookups);
+        let mylookups = this.$dataSource.load("Lookups");
+        console.log("mylookups:", mylookups);
+        try {
+            mylookups.getOne().then((data) => {
+                console.log("data:", data);
+
+                console.log(mylookups.List.menuItemTags);
+            });
+        } catch (e) {
+            console.log(e);
+        }
 
         // self.menuItemCategories = lookups.List.menuItemTags;
         // console.log("menuItemCategories:", self.menuItemCategories)
     };
-    
+
     // title, subtitle, items, footer
-    this.addSection = function(section){
+    this.addSection = function (section) {
         var newSection = {
             title: "Section Title",
             subtitle: "Section subtitle",
@@ -70,23 +86,23 @@ function tmMenuDocSvc (tmDocFactory) {
         this.getCategories();
         this.setActiveTab(1);
     };
-    
-    this.removeSection = function(index){
+
+    this.removeSection = function (index) {
         this.doc.sections.splice(index, 1);
     };
-    
-    this.updateSection = function(section){};
-    
-    this.addMenuItem = function(section, menuItem){};
-    
-    this.removeMenuItem = function(section, menuItem){};
-    
-    this.updateMenuItem = function(section, menuItem){};
-    
-    
-    
+
+    this.updateSection = function (section) { };
+
+    this.addMenuItem = function (section, menuItem) { };
+
+    this.removeMenuItem = function (section, menuItem) { };
+
+    this.updateMenuItem = function (section, menuItem) { };
+
+
+
     return this;
-    
+
 }
 
 
