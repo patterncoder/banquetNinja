64152a1b3756b42acc14ae72661ccdb8e245db29
trillobite <jparnell0@protonmail.com>
2021-05-26 14:33:44 -0700

    rentals are now alphabetical

--- app/features/events/rentalitems/tmRentalItemsCtrl.js
+++ app/features/events/rentalitems/tmRentalItemsCtrl.js
@@ -13,12 +13,70 @@ class tmRentalItemsCtrl {
         };
         
         this.__proto__ = tmListFactory(constructorArgs);
-        this.loadData();
+        // this.loadData();
         
-        this.sortOptions = [ { value: "name", text: "Sort by Item" }, { value: "meta.datecreated", text: "Sort by Date Created" }];
+        // this.sortOptions = [ { value: "name", text: "Sort by Item" }, { value: "meta.datecreated", text: "Sort by Date Created" }];
 
-        this.sortOrder = this.sortOptions[0].value;
+        // this.sortOrder = this.sortOptions[0].value;
 
+        this.activeFilter = "A";
+
+        this.activeRentals = [];
+
+        let alphaSorted = {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": [], "I": [], "J": [], "K": [], "L": [], "M": [], "N": [], "O": [], "P": [], "Q": [], "R": [], "S": [], "T": [], "U": [], "V": [], "W": [], "X": [], "Y": [], "Z": []};
+
+        console.log("tmRentalItemsCtrl this:", this);
+
+        this.stripNums = (rentObj) => {
+            let nwName = "";
+            if (rentObj.hasOwnProperty("name")) {
+                let name = rentObj.name;
+                for (let i = 0; i < name.length; ++i) {
+                    if (isNaN(name[i])) {
+                        nwName += name[i];
+                    }
+                }
+                console.log(nwName);
+            } else {
+                console.log("NO NAME PROPERTY!!!", rentObj);
+            }
+            return nwName;
+        };
+
+        this.getFilterChar = (nwName) => {
+            let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+            let str = nwName.toUpperCase();
+            let char = str[0];
+            if (alphabet.indexOf(char) > -1) {
+                return char;
+            }
+
+            return "A";
+        };
+
+
+        this.changeFilter = function (value) {
+            this.activeFilter = value.value;
+            this.activeRentals = alphaSorted[this.activeFilter];
+
+        };
+
+        this.afterLoad = () => {
+            //this.items.reverse() //comes in from db oldest first...
+
+            this.items.map((obj) => {
+                obj.nwName = this.stripNums(obj);
+                obj.filterChar = this.getFilterChar(obj.nwName);
+                alphaSorted[obj.filterChar].push(obj);
+            });
+
+            this.activeRentals = alphaSorted[this.activeFilter]; //Usually "A".
+        };
+
+        //loadData needs to be async or return a promise.
+        this.loadData().then((tmp) => {
+            console.log("loadData result:", tmp);
+        });
         
     }
     
