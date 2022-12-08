f1852cf3944bd54a2b29cfcf33aeba9cb57c9057
patterncoder <chris@baily.com>
2022-12-08 10:37:00 -0800

    use ninjaAPI for alpha select

--- app/features/production/menuitems/tmMenuItemsCtrl.js
+++ app/features/production/menuitems/tmMenuItemsCtrl.js
@@ -13,74 +13,60 @@ class tmMenuItemsCtrl {
         this.__proto__ = tmListFactory(constructorArgs);
         // this.loadData();
 
-        // this.sortOptions = [ { value: "name", text: "Sort by Item" }, { value: "meta.datecreated", text: "Sort by Date Created" }];
+        this.sortOptions = [ { value: "name", text: "Sort by Item" }, { value: "meta.datecreated", text: "Sort by Date Created" }];
 
-        // this.sortOrder = this.sortOptions[0].value;
-
-        this.activeFilter = "A";
-
-        this.activeMenuItems = [];
-
-        let alphaSorted = { "A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": [], "I": [], "J": [], "K": [], "L": [], "M": [], "N": [], "O": [], "P": [], "Q": [], "R": [], "S": [], "T": [], "U": [], "V": [], "W": [], "X": [], "Y": [], "Z": [], "*": [] };
-
-        this.stripNums = (menuObj) => {
-            let nwName = "";
-            if (menuObj.hasOwnProperty("name")) {
-                let name = menuObj.name;
-                for (let i = 0; i < name.length; ++i) {
-                    if (isNaN(name[i])) {
-                        nwName += name[i];
-                    }
-                }
-            } else {
-            }
-            return nwName;
-        };
-
-        this.getFilterChar = (nwName) => {
-            let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-            let str = nwName.toUpperCase();
-            let char = str[0];
-            if (alphabet.indexOf(char) > -1) {
-                return char;
-            }
-
-            return "A";
-        };
+        this.sortOrder = this.sortOptions[0].value;
 
 
+        
         this.changeFilter = function (value) {
-            //refresh the list, in case a new item was added.
-            alphaSorted = { "A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": [], "I": [], "J": [], "K": [], "L": [], "M": [], "N": [], "O": [], "P": [], "Q": [], "R": [], "S": [], "T": [], "U": [], "V": [], "W": [], "X": [], "Y": [], "Z": [], "*": [] };
-            this.activateAndSort();
-
-            this.activeFilter = value.value;
-            this.activeMenuItems = alphaSorted[this.activeFilter];
-
+            var filter = {
+                select: "name title description active",
+                "sort[name]": 1,
+                "startsWith[name]": value.value,
+            };
+            this.loadData(filter, true, true);
         };
 
-        this.activateAndSort = () => {
+        // this.activeFilter = "A";
 
-            if (this.items.length < 25) {
-                this.activeFilter = "*";
-            }
+        // this.activeMenuItems = [];
 
-            this.items.map((obj) => {
-                obj.nwName = this.stripNums(obj);
-                obj.filterChar = this.getFilterChar(obj.nwName);
-                alphaSorted[obj.filterChar].push(obj);
-                alphaSorted["*"].push(obj); //collect all into this one.
-            });
+        // let alphaSorted = { "A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": [], "I": [], "J": [], "K": [], "L": [], "M": [], "N": [], "O": [], "P": [], "Q": [], "R": [], "S": [], "T": [], "U": [], "V": [], "W": [], "X": [], "Y": [], "Z": [], "*": [] };
 
-            this.activeMenuItems = alphaSorted[this.activeFilter]; //Usually "A".
-        }
+        // this.stripNums = (menuObj) => {
+        //     let nwName = "";
+        //     if (menuObj.hasOwnProperty("name")) {
+        //         let name = menuObj.name;
+        //         for (let i = 0; i < name.length; ++i) {
+        //             if (isNaN(name[i])) {
+        //                 nwName += name[i];
+        //             }
+        //         }
+        //     } else {
+        //     }
+        //     return nwName;
+        // };
 
-        this.afterLoad = () => {
-            //this.items.reverse() //comes in from db oldest first...
-            this.activateAndSort();
-        };
+        // this.getFilterChar = (nwName) => {
+        //     let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        //     let str = nwName.toUpperCase();
+        //     let char = str[0];
+        //     if (alphabet.indexOf(char) > -1) {
+        //         return char;
+        //     }
 
-        this.loadData();
+        //     return "A";
+        // };
+
+
+        
+
+        this.loadData({
+            select: "name title description active",
+            "sort[name]": 1,
+            "startsWith[name]": "A",
+        }, true, true);
     }
 }
 
