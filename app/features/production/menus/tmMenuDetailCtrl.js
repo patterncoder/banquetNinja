18785797afa042a7c655e9d3a4a779a0bd442fe5
Chris Baily <patterncoder@gmail.com>
2025-02-10 10:54:31 -0800

    menu printing function

--- app/features/production/menus/tmMenuDetailCtrl.js
+++ app/features/production/menus/tmMenuDetailCtrl.js
@@ -37,6 +37,36 @@ function tmMenuDetailCtrl(
     });
 
 
+        this.moreFunctions.pdf = {
+            label: "Print Recipes",
+            method: function () {
+                let openPDF = () => {
+                    let url = `${config.apiBase}/production/menus/${self.$stateParams.id}/recipes-report`;
+                    var req = {
+                        method: 'GET',
+                        url: url,
+                        responseType: 'arraybuffer'
+                    };
+                    self.$http(req).then(function (result) {
+                        var file = new Blob([result.data], { type: 'application/pdf' });
+                        var fileURL = URL.createObjectURL(file);
+                        window.open(fileURL);
+                    });
+                };
+    
+                //lets save the contract before trying to print it!
+                self.setLoading(true);
+                self.docSvc.saveChanges().then(() => {
+                    openPDF();
+                    self.setLoading(false);
+                }, (err) => {
+                    self.tmNotifier.error("There was a problem with saving...");
+                    self.setLoading(false);
+                });
+            }
+        };
+
+
     this.moreFunctions.cloneMenu = {
       label: "Clone Menu",
       method: () => {
